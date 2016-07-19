fbd6ec29c0d9d163f63b50beaa606eb884da13b5
Peter Jacobs <thisispeterj@gmail.com>
2016-07-19 13:13:56 -0700

    refactor revealpoint function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,15 @@
-var revealPoint = function() {
+var animatePoints = function() {
     var points = document.getElementsByClassName('point');
-    for(var i = 0; i < points.length; i++) {
+    
+    var revealPoint = function(index) {
         points[i].style.opacity = 1;
         points[i].style.transform = "scaleX(1) translateY(0)";
         points[i].style.msTransform = "scaleX(1) translateY(0)";
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    };
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i)
+    }    
 };
-revealPoint();
+animatePoints();
