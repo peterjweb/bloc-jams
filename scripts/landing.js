93746e903801cac867742d51a390b911c4443356
Peter Jacobs <thisispeterj@gmail.com>
2016-07-19 14:54:30 -0700

    complete checkpoint-23-assignment-events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,12 @@
 var pointsArray = document.getElementsByClassName('point');
-
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 var animatePoints = function(points) {
-    var revealPoint = function(index) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i)
-    }    
+    forEach(pointsArray, revealPoint);
 };
 
 window.onload = function() {
