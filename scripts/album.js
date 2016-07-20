b5c9a70e19b62a8fea37647ff9d836bb4b5e20c4
Peter Jacobs <thisispeterj@gmail.com>
2016-07-20 16:44:27 -0700

    complete checkpoint assignment 25 - album view

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,10 @@
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumCover = document.getElementsByClassName('album-cover-art')[0];
+
 var albumPicasso = {
     title : "The Colors",
     artist : "Pablo Picasso",
@@ -28,6 +35,30 @@ var albumMarconi = {
     ]
 };
 
+var albumQOTSA = {
+	title : "Songs for the Deaf",
+	artist : "Queens Of The Stoneage",
+	label : "Interscope",
+	year : "2002",
+	albumArtUrl : "assets/images/album_covers/22.png",
+	songs : [
+		{ title : "You Think I Ain't Worth a Dollar, But I Feel Like a Millionaire", duration: "3:12" },
+		{ title : "No One Knows", duration: "4:38" },
+		{ title : "First It Giveth", duration: "3:18" },
+		{ title : "A Song for the Dead", duration: "5:52" },
+		{ title : "The Sky Is Fallin'", duration: "6:15" },
+		{ title : "Six Shooter", duration: "1:19" },
+		{ title : "Hangin' Tree", duration: "3:06" },
+		{ title : "Go with the Flow", duration: "3:09" },
+		{ title : "Gonna Leave You", duration: "2:50" },
+		{ title : "Do It Again", duration: "4:04" },
+		{ title : "God Is in the Radio", duration: "6:04" },
+		{ title : "Another Love Song", duration: "3:16" },
+		{ title : "A Song for the Deaf", duration: "6:42" },
+		{ title : "Mosquito Song", duration: "5:37" }
+	]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
 	var template = 
 		 '<tr class="album-view-song-item">'
@@ -40,13 +71,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 		return template;	
 };
 
-var setCurrentAlbum = function(album) {
-	var albumTitle = document.getElementsByClassName('album-view-title')[0];
-	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-	var albumImage = document.getElementsByClassName('album-cover-art')[0];
-	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-	
+var setCurrentAlbum = function(album) {	
 	albumTitle.firstChild.nodeValue = album.title;
 	albumArtist.firstChild.nodeValue = album.artist;
 	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -58,6 +83,18 @@ var setCurrentAlbum = function(album) {
 	}
 };
 
+var albumArray = [albumPicasso, albumMarconi, albumQOTSA];
+var index = 1;
+
 window.onload = function() {
-	setCurrentAlbum(albumMarconi);
+	setCurrentAlbum(albumPicasso);
+	
+	albumImage.addEventListener('click', function() {
+		setCurrentAlbum(albumArray[index]);
+		index ++;
+		
+		if (index >= albumArray.length) {
+			index = 0;
+		}
+	});	
 };
